fbfff717bcbd1ad42e7ba8a7fc0e1c3932892843
Tuan Ha <hatuan@email.cz>
2026-08-03 21:35:49 +0200

    Create scripts for create, insert and drop db

--- db/insert.js
+++ db/insert.js
@@ -11,6 +11,56 @@ const SQL_INSERT = `
   ('Dice', '1992-5-1', 'Sweden', 'Battlefield games', 'someurl', '2345', true, false),
   ('Warhorse', '2011-7-25', 'Czechia', NULL, 'someurl', '123445', true, false),
   ('CD Project RED', '1994-5-1', 'Poland', 'Kinda peak company', 'someurl', '1234', true, true);
+
+  INSERT INTO platforms (name)
+  VALUES
+  ('PlayStation'),
+  ('Xbox'),
+  ('Nintento'),
+  ('PC'),
+  ('Mobile');
+
+  INSERT INTO genres (name) 
+  VALUES
+  ('Action'),
+  ('Adventure'),
+  ('RPG'),
+  ('Soulslike'),
+  ('Strategy'),
+  ('Simulation'),
+  ('Sports'),
+  ('Racing'),
+  ('Puzzle'),
+  ('Platformer'),
+  ('Fighting'),
+  ('Shooter'),
+  ('Horror'),
+  ('Survival'),
+  ('Stealth'),
+  ('MMO'),
+  ('Card Game'),
+  ('Party'),
+  ('Sandbox'),
+  ('Visual Novel'),
+  ('Online game');
+
+  INSERT INTO games (name, release_date, description, image_file, developer_id, publisher_id)
+  VALUES
+  ('The Last of Us', '2013-6-14', 'A zombie horror story game', '123456', 1, 1),
+  ('Cyberpunk 2077', '2020-12-10', 'Futuristic dystopian role-playing game', '1234567', 5, 5),
+  ('Battlefield 1', '2016-10-21', 'FPS set in WW1', '1234', 3, NULL);
+
+  INSERT INTO games_genres (game_id, genre_id)
+  VALUES
+  (1, 1), (1, 2), (1, 13), (1, 14),
+  (2, 1), (2, 2), (2, 3), (2, 12),
+  (3, 12), (3, 21);
+
+  INSERT INTO games_platforms (game_id, platform_id)
+  VALUES
+  (1, 1),
+  (2, 1), (2, 2), (2, 4),
+  (3, 1), (3, 2), (3, 4)
   `;
 
 async function main() {
